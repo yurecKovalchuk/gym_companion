bb5c11a63f3e9829ec7b982996306553c828a8b2
yurecKovalchuk <KovalchukYurec@hotmail.com>
2023-07-31 10:50:58 +0300

    refactor: remove training state

--- lib/features/tasks/bloc/bloc.dart
+++ lib/features/tasks/bloc/bloc.dart
@@ -1,1 +1,2 @@
 export 'package:timer_bloc/features/tasks/bloc/training_bloc.dart';
+export 'package:timer_bloc/features/tasks/bloc/training_state.dart';
---
+++ lib/features/tasks/bloc/training_state.dart
@@ -0,0 +1,5 @@
+import 'package:timer_bloc/features/tasks/models/models.dart';
+
+class TrainingState {
+  final List<Exercise> exercises = [];
+}
