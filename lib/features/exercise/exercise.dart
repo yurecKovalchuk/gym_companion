6df212505b27769df2ef96a166f28ee35f4e3924
yurecKovalchuk <KovalchukYurec@hotmail.com>
2023-08-12 22:08:12 +0300

    refactor/delete unnessary folders

--- lib/features/exercise/exercise.dart
+++ lib/features/exercise/exercise.dart
@@ -1,3 +1,2 @@
 export 'package:timer_bloc/features/exercise/screen/exercise_screen.dart';
 export 'package:timer_bloc/features/exercise/bloc/bloc.dart';
-export 'package:timer_bloc/features/exercise/models/models.dart';
