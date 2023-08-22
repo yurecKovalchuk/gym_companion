eab69f5c76e74a106b43b63319f96741de89bce0
yurecKovalchuk <KovalchukYurec@hotmail.com>
2023-08-22 14:59:25 +0300

    refactor/chage show modal widget

--- lib/features/exercise_create/widget/show_modal_bottom_sheet_set_approaches.dart
+++ lib/features/exercise_create/widget/show_modal_bottom_sheet_set_approaches.dart
@@ -19,13 +19,6 @@ class ShowModalBottomSheetSetApproaches extends StatefulWidget {
 
 class _ShowModalBottomSheetSetApproachesState
     extends State<ShowModalBottomSheetSetApproaches> {
-  late final TextEditingController _textEditingController =
-      TextEditingController(
-    text: widget.value != null
-        ? widget.value.toString()
-        : _currentSliderValue.toString(),
-  );
-
   late ApproachType selectedType = widget.type == ApproachType.exercise
       ? ApproachType.exercise
       : ApproachType.rest;
@@ -50,132 +43,178 @@ class _ShowModalBottomSheetSetApproachesState
           ),
           color: Colors.white,
         ),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 100,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  _currentSliderValue.toString(),
-                  style: const TextStyle(
-                    fontSize: 36,
-                  ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: ' Add your new ',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleLarge
+                                ?.copyWith(
+                                    color: Colors.black.withOpacity(0.5)),
+                          ),
+                          const TextSpan(
+                            text: '\n', // New line
+                          ),
+                          TextSpan(
+                              text: selectedType == ApproachType.rest
+                                  ? 'Rest'
+                                  : 'Exercise',
+                              style: Theme.of(context).textTheme.headlineLarge),
+                          TextSpan(
+                            text: ' of ',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleLarge
+                                ?.copyWith(
+                                    color: Colors.black.withOpacity(0.5)),
+                          ),
+                          TextSpan(
+                              text: _currentSliderValue.toString(),
+                              style: Theme.of(context).textTheme.headlineLarge),
+                          TextSpan(
+                            text: ' seconds ',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleLarge
+                                ?.copyWith(
+                                    color: Colors.black.withOpacity(0.5)),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                const Text(
-                  'seconds',
-                  style: TextStyle(fontSize: 18),
-                )
-              ],
-            ),
-            Slider(
-              value: _currentSliderValue.toDouble(),
-              max: 120,
-              divisions: 5,
-              label: _currentSliderValue.round().toString(),
-              onChanged: (double value) {
-                setState(() {
-                  _currentSliderValue = value as int;
-                });
-              },
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                GestureDetector(
-                  onTap: selectedTypeApproachExercise,
-                  child: Container(
-                    height: 60,
-                    width: 140,
-                    decoration: BoxDecoration(
-                      color: selectedType == ApproachType.exercise
-                          ? Colors.green
-                          : Colors.transparent,
-                      borderRadius: BorderRadius.circular(8.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: selectedType == ApproachType.exercise
-                              ? Colors.black.withOpacity(0.2)
-                              : Colors.transparent,
-                          spreadRadius: 2,
-                          blurRadius: 5,
-                          offset: const Offset(0, 3),
+              ),
+              const Divider(),
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 8.0,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Card(
+                        color: selectedType == ApproachType.exercise
+                            ? Colors.green
+                            : Colors.transparent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          side: BorderSide(
+                            color: selectedType == ApproachType.exercise
+                                ? Colors.black.withOpacity(0.2)
+                                : Colors.transparent,
+                            width: 2.0,
+                          ),
                         ),
-                      ],
-                    ),
-                    child: const Center(
-                      child: Text('Exercise'),
-                    ),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: selectedTypeApproachRest,
-                  child: Container(
-                    height: 60,
-                    width: 140,
-                    decoration: BoxDecoration(
-                      color: selectedType == ApproachType.rest
-                          ? Colors.red
-                          : Colors.transparent,
-                      borderRadius: BorderRadius.circular(8.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: selectedType == ApproachType.rest
-                              ? Colors.black.withOpacity(0.2)
-                              : Colors.transparent,
-                          spreadRadius: 2,
-                          blurRadius: 5,
-                          offset: const Offset(0, 3),
+                        child: InkWell(
+                          onTap: selectedTypeApproachExercise,
+                          child: const SizedBox(
+                            height: 40,
+                            child: Center(
+                              child: Text('Exercise'),
+                            ),
+                          ),
                         ),
-                      ],
+                      ),
                     ),
-                    child: const Center(
-                      child: Text('Rest'),
+                    Expanded(
+                      child: Card(
+                        color: selectedType == ApproachType.rest
+                            ? Colors.red
+                            : Colors.transparent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          side: BorderSide(
+                            color: selectedType == ApproachType.exercise
+                                ? Colors.black.withOpacity(0.2)
+                                : Colors.transparent,
+                            width: 2.0,
+                          ),
+                        ),
+                        child: InkWell(
+                          onTap: selectedTypeApproachRest,
+                          child: const SizedBox(
+                            height: 40,
+                            child: Center(
+                              child: Text('Rest'),
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
-            const SizedBox(
-              height: 36,
-            ),
-            const Spacer(),
-            SizedBox(
-              width: double.infinity,
-              height: 40,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(18),
-                          topRight: Radius.circular(18)),
+              ),
+              Row(
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      text: ' Duration: ',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleLarge
+                          ?.copyWith(color: Colors.black.withOpacity(0.5)),
                     ),
+                  )
+                ],
+              ),
+              Slider(
+                value: _currentSliderValue.toDouble(),
+                max: 120,
+                divisions: 5,
+                label: _currentSliderValue.round().toString(),
+                onChanged: (double value) {
+                  setState(() {
+                    _currentSliderValue = value as int;
+                  });
+                },
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 16.0,
                   ),
-                  onPressed: () {
-                    setState(() {
-                      final timer = _currentSliderValue.toString();
-                      Navigator.pop(context, {
-                        'timer': timer,
-                        'type': selectedType,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(18),
+                            topRight: Radius.circular(18)),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        final timer = _currentSliderValue.toString();
+                        Navigator.pop(context, {
+                          'timer': timer,
+                          'type': selectedType,
+                        });
                       });
-                    });
-                  },
-                  child: Text(
-                    widget.type == null && widget.value == null
-                        ? 'Add'
-                        : 'Update',
+                    },
+                    child: Text(
+                      widget.type == null && widget.value == null
+                          ? 'Add'
+                          : 'Update',
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
