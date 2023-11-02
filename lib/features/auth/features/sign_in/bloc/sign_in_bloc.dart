64fddb57689efa8db058b0a3b788b504ba66fcfd
yurecKovalchuk <KovalchukYurec@hotmail.com>
2023-11-02 13:52:38 +0200

    refactor/ change variable names, add token validation to understand if the user is already registered

--- lib/features/auth/features/sign_in/bloc/sign_in_bloc.dart
+++ lib/features/auth/features/sign_in/bloc/sign_in_bloc.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'package:timer_bloc/datasource/datasource.dart';
 import 'package:timer_bloc/models/models.dart';
 import 'package:timer_bloc/exceptions/exceptions.dart';
+import 'package:timer_bloc/repository/repository.dart';
 import '../sign_in.dart';
 
 class SignInBloc extends Cubit<SignInState> {
@@ -27,11 +27,11 @@ class SignInBloc extends Cubit<SignInState> {
         _repository.saveToken(response.token!);
         emit(state.copyWith(status: SignInStatus.success));
       } on ValidationException catch (e) {
-        emit(state.copyWith(status: SignInStatus.error, error: e.response.message.toString()));
+        emit(state.copyWith(status: SignInStatus.error, errorMessage: e.response.message.toString()));
       } catch (e) {
         emit(state.copyWith(
           status: SignInStatus.error,
-          error: e.toString(),
+          errorMessage: e.toString(),
         ));
       }
     }
